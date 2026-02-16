599e635da56feffc52f4f20ebbb3352b3ef44f23
SohailShafiq1 <Sohail.shafiq002@gmail.com>
2026-02-16 21:00:54 +0500

    new netlify

--- Server.js
+++ Server.js
@@ -18,8 +18,9 @@ const PORT = process.env.PORT || 5000;
 
 // -------------------- CORS SETUP --------------------
 const allowedOrigins = [
-  'http://localhost:5173', //  
-  'https://ustadwaseemjuttkotla.com/'// production
+  'http://localhost:5173',          // local dev
+  'https://ustadwaseemjuttkotla.com', // production, **no trailing slash**
+  'https://ustadwaseemjuttkotla.netlify.app' // if you use Netlify preview
 ];
 
 const corsOptions = {
